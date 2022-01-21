087c84890116c5856cfdc57337206ceeead02fed
Kellie Matteson <kelliematteson@yahoo.com>
2022-01-20 22:21:11 -0800

    changed mongo

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const app = express();
 const PORT = process.env.PORT || 3000;
-const mongoURI = process.env.MONGO_URI;
+const mongoURI = process.env.MONGODB_URI;
 const Clean = require('./models/clean');
 
 // Middleware Below
